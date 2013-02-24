8624a8c49e21a54cfb51f213f6207a496fc22da1
Idan Gazit <idan@gazit.me>
2013-02-24 20:23:58 +0200

    Tweaking and cleaning up user-data script

--- aws_bootstrap.sh
+++ aws_bootstrap.sh
@@ -1,15 +1,27 @@
+#!/bin/bash
+set -e -x
+
+# update and install various bits we need
 apt-get -y update
 apt-get -y upgrade
-
 apt-get -y install build-essential python2.7-dev git libxml2 libxml2-dev libxml2-utils libxslt1.1 libxslt1-dev libzmq1 libzmq-dev tmux
 
-mkdir -p /mnt/data
-mount /dev/xvdc /mnt/data
-
+# setup python and pip requirements
 curl -O http://python-distribute.org/distribute_setup.py
 python distribute_setup.py
 easy_install pip
-pip install -r /mnt/data/polyglots/requirements.txt
 pip install ipython tornado pyzmq
 
-echo 'export IPYTHONDIR=/mnt/data/.ipython' > /home/ubuntu/.bashrc
+# git clone our stuff
+echo 'export IPYTHONDIR=~/polyglots/.ipython' >> /home/ubuntu/.bashrc
+echo 'export POLYGLOTS_REPOS=/mnt/data/repos' >> /home/ubuntu/.bashrc
+sudo -u ubuntu -i git clone https://github.com/umbrellaco/polyglots.git
+pip install -r /home/ubuntu/polyglots/requirements.txt
+
+sudo -u ubuntu -i tmux new-session -d
+
+# mount the snapshot of the git repos
+mkdir -p /mnt/data
+mount -r -o noatime,nodiratime,noacl,nouser_xattr /dev/xvdc /mnt/data
+
+
